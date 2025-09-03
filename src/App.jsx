2f5274435c5807fea66e94db6dab70d9287d2e52
RiyaRoy05 <riyaroy9747r@gmail.com>
2025-09-03 16:26:57 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/sign-in' element={<SignUp/>}></Route>
+        <Route path='/sign-in' element={<Signin/>}></Route>
         <Route path='/sign-up' element={<SignUp/>}></Route>
         <Route path='/about' element={<About/>}></Route>    
         <Route path='/profile' element={<Profile/>}></Route>
